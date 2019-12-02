d9fb6d9181835ebb3e33f974cbe2841eb649c8a3
TAISUKE Fujii <fuzzybsd@gmail.com>
2019-12-02 17:41:41 +0900

    step1を作った。

--- app/app.js
+++ app/app.js
@@ -1,19 +1,8 @@
 const express = require('express');
 var app = express();
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'mysql',
-    user: 'docker',
-    password: 'docker',
-    database: 'test_database'
-});
-
 app.get('/', function (req, res) {
-    connection.query('SELECT * FROM `HOGE`', function (error, results, fields) {
-        if (error) throw error;
-        res.send(results[0]);
-    });
+    res.send('Hello World!');
 });
 
 app.listen(3000, function () {
--- db/sql/create-hoge.sql
+++
@@ -1,6 +0,0 @@
-create table HOGE (
-  `id` INT,
-  `name` VARCHAR(100)
-);
-
-insert into HOGE (`id`, `name`) values (1, 'Hello World!');
